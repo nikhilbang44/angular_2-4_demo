1dc61663436412116c9383f9a19196644dc8d193
Nikhil44 <nbang0506@gmail.com>
2018-02-12 14:51:35 +0530

    integration of all

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   name = '';
+  showSecret = false;
+  log = [];
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    this.log.push(new Date());
+  }
 }
