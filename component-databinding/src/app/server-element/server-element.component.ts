535c3fa771e58baefaf059cab21ec494b21a5e52
Nikhil44 <nbang0506@gmail.com>
2018-02-17 23:49:54 +0530

    Binding to Custom Events

--- component-databinding/src/app/server-element/server-element.component.ts
+++ component-databinding/src/app/server-element/server-element.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string} ;
+
   constructor() { }
   ngOnInit() {
   }
+  
 }
