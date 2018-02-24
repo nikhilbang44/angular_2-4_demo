a5802702f3f8a5cffd74d437067f9f3f2ab79251
Nikhil44 <nbang0506@gmail.com>
2018-02-24 23:58:08 +0530

    Injecting Services into Services

--- serviceExaple/src/app/account/account.component.ts
+++ serviceExaple/src/app/account/account.component.ts
@@ -1,22 +1,18 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { LoggingService } from '../logging.service';
 import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   
- constructor(private loggingService: LoggingService,
-             private accountService: AccountsService){}
+ constructor(private accountService: AccountsService){}
 
   onSetTo(status: string) {
     this.accountService.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status); 
   }
 }
